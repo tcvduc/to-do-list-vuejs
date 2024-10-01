8ceecfc48616dfab83321a6b69ff1ba7293e32e9
tcvduc <caovanducs@gmail.com>
2024-10-01 08:23:55 +0700

    onboard css

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,16 +19,36 @@ li {
 a {
   color: #42b983;
 }
+
+.tu {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.hai {
+  margin: 24px;
+}
+
+.hai:nth-child(1) {
+  margin: 24px 0px 24px 24px;
+}
 </style>
 
 <!-- web code  -->
 
 <template>
   <div class="hello">
-    <h1>{{ hocVue }}</h1>
-    <h1>{{ chaoBuoiSang }}</h1>
-    <h1 style="color: burlywood">{{ lamMotLyCaPhe }}</h1>
-    <h1>{{ lamMotOBanhMy }}</h1>
+    <div class="tu">
+      <div class="hai">
+        <h1>{{ hocVue }}</h1>
+        <h1>{{ chaoBuoiSang }}</h1>
+      </div>
+      <div class="hai">
+        <h1 style="color: burlywood">{{ lamMotLyCaPhe }}</h1>
+        <h1>{{ lamMotOBanhMy }}</h1>
+      </div>
+    </div>
   </div>
 </template>
 
