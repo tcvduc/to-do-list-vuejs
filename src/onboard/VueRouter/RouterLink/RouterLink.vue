416d6532e68b9463093cc4082c0320f1f90b99ce
tcvduc <caovanducs@gmail.com>
2024-10-07 17:08:30 +0700

    rest

---
+++ src/onboard/VueRouter/RouterLink/RouterLink.vue
@@ -0,0 +1,19 @@
+<style>
+.routerLinkFrame {
+  display: flex;
+}
+</style>
+
+<template>
+  <div class="routerLinkFrame">
+    <div class="routerLinkTitle">Router Link</div>
+    <div class="routers">
+      <div class="router">Home</div>
+      <div class="router">Header</div>
+      <div class="router">Content</div>
+      <div class="router">Footer</div>
+    </div>
+  </div>
+</template>
+
+<script></script>
